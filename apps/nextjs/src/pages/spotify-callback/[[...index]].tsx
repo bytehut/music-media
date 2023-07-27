2d619166fc164475f64453d2c5724d99adac6d18
darren <darrenh1@cs.washington.edu>
2023-07-27 06:29:48 -0700

    added state to spotify callback component

--- apps/nextjs/src/pages/spotify-callback/[[...index]].tsx
+++ apps/nextjs/src/pages/spotify-callback/[[...index]].tsx
@@ -2,27 +2,38 @@ import { useRouter } from "next/router";
 import { trpc } from "../../utils/trpc";
 import { TRPCError } from "@trpc/server";
 import { LoadingAnimation } from "../../components/loading";
+import { useEffect, useState } from "react";
 
 const SpotifyCallback = () => {
   const router = useRouter();
   const { code, state } = router.query;
-  if (
-    code === undefined ||
-    typeof code !== "string" ||
-    state === undefined ||
-    typeof state !== "string"
-  ) {
-    throw new TRPCError({
-      code: "INTERNAL_SERVER_ERROR",
-      message: "code or state not a string",
+
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState<string | undefined>(undefined);
+
+  useEffect(() => {
+    if (
+      code === undefined ||
+      typeof code !== "string" ||
+      state === undefined ||
+      typeof state !== "string"
+    ) {
+      throw new TRPCError({
+        code: "INTERNAL_SERVER_ERROR",
+        message: "code or state not a string",
+      });
+    }
+    const { data, isLoading } = trpc.spotifyAuth.handleCallback.useQuery({
+      code,
+      state,
     });
-  }
-  const { data, isLoading } = trpc.spotifyAuth.handleCallback.useQuery({
-    code,
-    state,
-  });
+    setLoading(isLoading);
+    setData(data);
+  }, [code, state]);
+
   if (isLoading) return <LoadingAnimation />;
   if (!data) return <div>Something went wrong</div>;
+
   return (
     <main className="flex h-screen flex-col items-center">
       <div className="flex flex-col items-center justify-center gap-4">
